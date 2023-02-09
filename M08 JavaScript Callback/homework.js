f99ce1727a865b8d559d20ade7a5e704f2f5ce2f
martinalonsonu <martin99huacho@gmail.com>
2023-02-09 17:52:51 -0500

    prep course terminado

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -5,12 +5,18 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+   let primeraLetra = nombre[0].toUpperCase();
+   for(let i = 1; i < nombre.length; i++){
+      primeraLetra += nombre[i];
+   }
+   return primeraLetra;
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   cb();
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -18,6 +24,7 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   return cb(num1, num2);
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -26,6 +33,11 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   let suma = 0;
+   for(let i = 0; i < arrayOfNumbers.length; i++){
+      suma = suma + parseInt(arrayOfNumbers[i]);
+   }
+   cb(suma);
 }
 
 function forEach(array, cb) {
@@ -33,6 +45,10 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+ //  for (let i = 0; i < array.length; i++){
+   //   cb(array[i]);
+   //}
+   array.forEach(cb);
 }
 
 function map(array, cb) {
@@ -40,12 +56,30 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+   //let nuevoArray = [];
+   //for(let i = 0; i < array.length; i++){
+   //   let resultado = cb(array[i]);
+   //   nuevoArray.push(resultado);
+   //}
+   //return nuevoArray;
+
+   // let nuevoArreglo = [];
+   // array.forEach(element => {
+   //    nuevoArreglo.push(cb(element));
+   // })
+   // return nuevoArreglo;
+
+   let newArray = array.map(e => cb(e))
+
+   return newArray;
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   return arrayOfStrings.filter(e => e.charAt(0) === "a")
+   
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,12 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   let array = [];
+   for (let key in objeto) {
+      array.push([key, objeto[key]]);
+   }
+
+   return array;
 }
 
 function numberOfCharacters(string) {
@@ -14,6 +20,17 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+   let objeto = {}
+   let array = string.split('').sort();
+   for (let letra of array) {
+      if (!objeto[letra]) {
+         objeto[letra] = 1;
+      } else {
+         objeto[letra] += 1;
+      }
+   }
+
+   return objeto;
 }
 
 function capToFront(string) {
@@ -22,6 +39,17 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+   var mayuscula = '';
+   var minuscula = '';
+   for (i = 0; i < string.length; i++) {
+      if (string[i] === string[i].toUpperCase()) {
+         mayuscula = mayuscula + string[i];
+      }
+      else {
+         minuscula = minuscula + string[i];
+      }
+   }
+   return (mayuscula + minuscula)
 }
 
 function asAmirror(frase) {
@@ -29,18 +57,41 @@ function asAmirror(frase) {
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
+   let nuevafrase = '';
+   array = frase.split(' ');
+   for(i = 0; i < array.length; i++){
+      if (i !== 0){
+         nuevafrase = nuevafrase + " " + array[i].split('').reverse().join('');
+      } else {
+         nuevafrase = nuevafrase + array[i].split('').reverse().join('');
+      }
+   }
+   return nuevafrase;
 }
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
    // Tu código:
+   let nuevoNumero = String(numero).split('').reverse().join('');
+   if(numero == nuevoNumero){
+      return "Es capicua";
+   } else{
+      return "No es capicua";
+   }
 }
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   let newString = '';
+   for(let i = 0; i < string.length; i++){
+      if(string[i] !== 'a' && string[i] !== 'b' && string[i] !== 'c'){
+         newString += string[i];
+      }
+   }
+   return newString;
 }
 
 function sortArray(arrayOfStrings) {
@@ -49,6 +100,7 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   return arrayOfStrings.sort((a,b) => a.length - b.length)
 }
 
 function buscoInterseccion(array1, array2) {
@@ -58,6 +110,15 @@ function buscoInterseccion(array1, array2) {
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
    // Tu código:
+   let newArray = []
+   for (let i = 0; i < array1.length; i++) {
+      for (let j = 0; j < array2.length; j++) {
+         if (array1[i] === array2[j]) {
+            newArray.push(array1[i]);
+         }
+      }
+   }
+   return newArray;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
